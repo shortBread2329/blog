696d286f4958f761032dc533292abf44a36cf365
shortBread2329 <shortBread1992@outlook.jp>
2021-08-10 04:15:25 +0900

    Add:20210810分下書き

---
+++ content/post/20210810/index.md
@@ -0,0 +1,10 @@
+---
+title: "20210810"
+date: 2021-08-10T04:15:21+09:00
+categories : [
+]
+tags : [
+]
+draft: true
+---
+
--- open.sh
+++ open.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-TODAY="`date +%Y%m%d`"
-NEWDIR=$TODAY
-# ディレクトリが存在するか
-for ((i=9; i>=0; i--)); do
-    NEWDIR="${TODAY}_${i}"
-    if [ -d ./content/post/$NEWDIR/ ]; then
-        break
+for ((j=0; j<2; j++)); do
+    TODAY=`date +%Y%m%d --date '${j} day ago'`
+    NEWDIR=$TODAY
+    # ディレクトリが存在するか
+    for ((i=9; i>=0; i--)); do
+        NEWDIR="${TODAY}_${i}"
+        if [ -d ./content/post/$NEWDIR/ ]; then
+            start ./content/post/$NEWDIR/index.md
+            exit 0
+        fi
+    done
+    if [ $NEWDIR ==  "${TODAY}_0" ]; then
+        NEWDIR=$TODAY
+        if [ -d ./content/post/$NEWDIR/ ]; then
+            start ./content/post/$NEWDIR/index.md
+            exit 0
+        fi
     fi
 done
-if [ $NEWDIR ==  "${TODAY}_0" ]; then
-    NEWDIR=$TODAY
-fi
-start ./content/post/$NEWDIR/index.md
+exit 1
